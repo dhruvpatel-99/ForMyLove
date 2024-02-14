46e95054680b014e663afbd63a2e421ef0c7bc72
dhruvpatel-99 <159978436+dhruvpatel-99@users.noreply.github.com>
2024-02-14 01:20:42 -0500

    Update main.js

--- script/main.js
+++ script/main.js
@@ -103,7 +103,7 @@ const animationTimeline = () => {
     .from(".idea-1", 1.0, ideaTextTrans)
     .to(".idea-1", 1.0, ideaTextTransLeave, "+=2.0")
     .from(".idea-2", 1.0, ideaTextTrans)
-    .to(".idea-2", 1.0, ideaTextTransLeave, "+=2.5")
+    .to(".idea-2", 1.0, ideaTextTransLeave, "+=2.0")
     .from(".idea-3", 1.2, ideaTextTrans)
     .to(".idea-3 strong", 0.75, {
       scale: 1.2,
@@ -111,7 +111,7 @@ const animationTimeline = () => {
       backgroundColor: "rgb(21, 161, 237)",
       color: "#fff",
     })
-    .to(".idea-3", 1.1, ideaTextTransLeave, "+=2.5")
+    .to(".idea-3", 1.1, ideaTextTransLeave, "+=2.0")
     .from(".idea-4", 1.0, ideaTextTrans)
     .to(".idea-4", 1.0, ideaTextTransLeave, "+=2.0")
     .from(
